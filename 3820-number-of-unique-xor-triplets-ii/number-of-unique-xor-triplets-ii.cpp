b4d11aaddf741627810587f1e08ed97cec194f32
Aman Singh <conqueror1703@gmail.com>
2026-07-24 20:12:55 +0530

    Time: 634 ms (64.61%) | Memory: 35.3 MB (80.90%) - LeetSync

--- 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        int n=nums.size();
-       unordered_set<int>s1;
-          unordered_set<int>s2;
-          for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                s1.insert(nums[i]^nums[j]);
+          int n = nums.size();
+        vector<bool> s1(1<<11,0);
+        vector<bool> s2(1<<11, 0);
 
-            }
-          }
-          for(auto pairs: s1){
-            for(int i=0;i<n;i++){
-                s2.insert(pairs^nums[i]);
-            }
-          }
-          return s2.size();
+      
 
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                s1[nums[i] ^ nums[j]] = 1;
+            }
+        }// all possible pair of xor
+
+        for (int x = 0; x < 1<<11; x++) {
+            if (!s1[x]) continue;
+
+            for (int i = 0; i < n; i++) {
+                s2[x ^ nums[i]] = 1;
+            }
+        }
+
+     return count(s2.begin(), s2.end(), 1);;
     }
 };
